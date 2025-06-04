64a3ce290233a54fef6cf4c73f6f40c8e24bcfb1
Shalini Kumari <147317106+snpathaks@users.noreply.github.com>
2025-06-04 22:54:49 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,3 +1,25 @@
+// Array Initialisation::
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+       cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+       cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+} */
+ 
+
+
+
+
+
 //Sorting an array..........................................
 /*
 #include<iostream>
@@ -336,6 +358,8 @@ int main () {
 		cout<<a[i]<<" ";
 		}
 }*/
+
+
 
 
 
